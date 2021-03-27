7937005e9cd6ef92f6e56fab4f1416d24d3a4957
shaheer <72137242+mshaheerz@users.noreply.github.com>
2021-03-27 11:59:50 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-echo "cheking dipentancy"
-sleep 4
+echo "cheking dipentancy...."
+sleep 3
 echo "......."
 apt-get install figlet
 figlet shaheerez
+read "input your number now :" number
+echo "your number is $number"
+
+ 
